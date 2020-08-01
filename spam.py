69e9d889208c9bcf9c9dc1a89d96c8271db9445a
swagkarna <46685308+swagkarna@users.noreply.github.com>
2020-08-01 07:26:25 -0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,54 @@
+import threading
+import subprocess
+import keyboard
+import time
+from colorama import Fore, Style,init
+time.sleep(2)
+def install():
+    try:
+       subprocess.call("pip3 install keyboard", shell=True)
+       subprocess.call("pip3 install colorama", shell=True)
+    except:
+        print("Something went wrong")
+install()
+banner = """
+
+
+ #####  ######     #    #     # ######  ####### ####### 
+ #     # #     #   # #   ##   ## #     # #     #    #    
+ #       #     #  #   #  # # # # #     # #     #    #    
+  #####  ######  #     # #  #  # ######  #     #    #    
+       # #       ####### #     # #     # #     #    #    
+ #     # #       #     # #     # #     # #     #    #    
+  #####  #       #     # #     # ######  #######    #    
+                                                     
+
+ 
+                          AUTHOR       :   swagkarna
+
+                          CONTACT ME   :   github.com/swagkarna
+
+"""
+print(Style.BRIGHT+Fore.RED+banner+Fore.RESET)
+
+
+
+      
+
+      
+def spam():
+    while True:
+       time.sleep(1)
+       file=open("spam.txt", 'r')
+       x=file.read()
+       keyboard.write(x)
+       time.sleep(2)
+       keyboard.send("enter")
+                     
+spam()         
+
+
+
+
+
+
